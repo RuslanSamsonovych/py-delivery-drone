e1aba3d19e3ada0b184897aa398556564fe69a49
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2026-07-01 13:50:19 +0300

    implement Delivery Drone

--- app/main.py
+++ app/main.py
@@ -46,14 +46,14 @@ class DeliveryDrone(FlyingRobot):
         name: str,
         weight: int,
         max_load_weight: int,
-        current_load: Cargo | None = None,
+        current_load: Cargo | None,
         coords: list[int] | None = None,
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
-        if self.current_load:
-            self.hook_load(self.current_load)
+        self.current_load = None
+        if current_load:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
